642a5f51c7c9c29004c6b701f5f4089c74e7447d
hao123 <henry.hsu@hy-tech.com.tw>
2018-06-21 10:42:31 +0800

    還是從55分開始重整好了

--- shopee.js
+++ shopee.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 
 //設定倒數計時             年,(n-1)月,日,時,分
-var timeout = new Date(2018, 5, 21, 11, 59, 50).getTime() - new Date().getTime();
+var timeout = new Date(2018, 5, 21, 11, 59, 55).getTime() - new Date().getTime();
 function delay() {
     return new Promise(function (reolve) {
         setTimeout(reolve, timeout);
